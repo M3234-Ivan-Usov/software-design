4491d802ee25021cab548f71ad116301342ccab1
Para1lax <83034393+Para1lax@users.noreply.github.com>
2021-10-10 12:00:40 +0300

    Add integration test

---
+++ java/refactoring/src/test/java/ru/akirakozov/sd/refactoring/MainTest.java
@@ -0,0 +1,89 @@
+package ru.akirakozov.sd.refactoring;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.*;
+import java.sql.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MainTest {
+    private static final Thread testServer = new Thread(() -> {
+        try {
+            Main.main(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    });
+
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    @BeforeClass
+    public static void initServer() throws InterruptedException {
+        testServer.start();
+        Thread.sleep(1000);  // Waiting for server to start
+    }
+
+    private HttpResponse<String> connectTo(String postfix)
+            throws IOException, InterruptedException, URISyntaxException {
+        HttpRequest request = HttpRequest.newBuilder(
+                new URL("http://localhost:8081/" + postfix).toURI()
+        ).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        return response;
+    }
+
+    @Test
+    public void test01AddProductConnection()
+            throws IOException, URISyntaxException, InterruptedException {
+        connectTo("add-product?name=iphone6&price=300");
+        connectTo("add-product?name=iphone7&price=500");
+    }
+
+    @Test
+    public void test02GetProductsConnection()
+            throws IOException, URISyntaxException, InterruptedException {
+        HttpResponse<String> response = connectTo("get-products");
+        Document doc = Jsoup.parse(response.body());
+        assertEquals("iphone6 300 iphone7 500", doc.body().text());
+    }
+
+    private void makeQuery(String command, String expectedResult)
+            throws IOException, URISyntaxException, InterruptedException {
+        HttpResponse<String> response = connectTo("query?command=" + command);
+        Document doc = Jsoup.parse(response.body());
+        assertEquals(expectedResult, doc.body().text());
+    }
+
+    @Test
+    public void test03QueryConnection() throws IOException, URISyntaxException, InterruptedException {
+        makeQuery("sum", "Summary price: 800");
+        makeQuery("min", "Product with min price: iphone6 300");
+        makeQuery("max", "Product with max price: iphone7 500");
+        makeQuery("count", "Number of products: 2");
+    }
+
+    @AfterClass
+    public static void shutdownServer() {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "DELETE FROM PRODUCT";
+            Statement stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (SQLException e) {
+            System.err.println("Failed to clear test database");
+            e.printStackTrace();
+        }
+        testServer.interrupt();
+    }
+}
